635e26e3ba71295962ecf8268a657537968cc9f4
Andrew Allen <andrewallen2012@gmail.com>
2013-07-15 22:14:13 -0500

    detailed alerts, room for improvement

--- lib/cta/alerts/details.rb
+++ lib/cta/alerts/details.rb
@@ -1,11 +1,31 @@
 module Cta
   module Alerts
-    class Details
-      attr_accessor :error_code, :routes
+    class DetailsService
+      attr_accessor :name, :route_type, :route_id, :description
+
       def initialize( args )
-        @response = Alerts.fetch('alerts.aspx', query: args).parsed_response
-        @error_code = @response["CTARoutes"]["ErrorCode"].to_i
-        # @routes = build_routes(@response["CTARoutes"]["RouteInfo"])
+        @name         = args["ServiceName"]
+        @route_type   = args["ServiceType"]
+        @route_id     = args["ServiceId"]
+        @description  = args["Train Route"]
+      end
+    end
+
+    class DetailsAlert
+      attr_accessor :alert_id, :headline, :short_description, :full_description, :severity_score, :impact, :event_start, :event_end, :tbd, :major_alert, :routes_affected
+      
+      def initialize( args )
+        @alert_id           = args["AlertId"].to_i
+        @headline           = args["Headline"]
+        @short_description  = args["ShortDescription"]
+        @full_description   = args["FullDescription"]
+        @severity_score     = args["SeverityScore"]
+        @impact             = args["Impact"]
+        @event_start        = parse_time args["EventStart"]
+        @event_end          = parse_time args["EventEnd"]
+        @tbd                = string_to_bool args["TBD"]
+        @major_alert        = string_to_bool args["MajorAlert"]
+        @routes_affected    = build_routes(args["ImpactedService"]["Service"])
       end
 
       private
@@ -13,8 +33,35 @@ module Cta
         input.kind_of?(Array) ? input : [input]
       end
 
-      def build_routes hash
-        ensure_array(hash).map { |route_info| OverviewRoute.new route_info }
+      def string_to_bool string
+        string == "0" ? false : true
+      end
+
+      def build_routes( hash )
+        ensure_array(hash).map { |route_info| DetailsService.new route_info }
+      end
+
+      def parse_time string
+        return nil if string.nil?
+        Time.parse(string)
+      end
+    end
+
+    class Details
+      attr_accessor :error_code, :alerts
+      def initialize( args )
+        @response = Alerts.fetch('alerts.aspx', query: args).parsed_response
+        @error_code = @response["CTAAlerts"]["ErrorCode"].to_i
+        @alerts = build_alerts(@response["CTAAlerts"]["Alert"])
+      end
+
+      private
+      def ensure_array input
+        input.kind_of?(Array) ? input : [input]
+      end
+
+      def build_alerts hash
+        ensure_array(hash).map { |alert_info| DetailsAlert.new alert_info }
       end
     end
   end
--- spec/alerts_spec.rb
+++ spec/alerts_spec.rb
@@ -33,9 +33,57 @@ end
 
 describe Cta::Alerts::Details do
   let(:details) { described_class.new routeid: 'red,p' }
-  let(:first_route) { details.routes.first }
+  let(:first_alert) { details.alerts.first }
 
   it "should return an error code of 0" do
     details.error_code.should be(0)
   end
+
+  it "should have alert id" do
+    first_alert.alert_id.should be_a(Integer)
+  end
+
+  it "should have headline" do
+    first_alert.headline.should be_a(String)
+  end
+
+  it "should have short description" do
+    first_alert.short_description.should be_a(String)
+  end 
+
+  it "should have full_description" do
+    first_alert.full_description.should be_a(String)
+  end 
+
+  it "should have severity_score" do
+    first_alert.severity_score.should be_a(String)
+  end 
+
+  it "should have impact" do
+    first_alert.impact.should be_a(String)
+  end 
+
+  it "should have event_start" do
+    first_alert.event_start.should be_a(Time)
+  end 
+
+  it "should have event_end, or nil" do
+    first_alert.event_end.should satisfy{|s| s.is_a?(Date) || s.nil?}
+  end  
+
+  # it "should have tbd" do
+  #   first_alert.tbd.should be_true or first_alert.tbd.should be_false
+  # end
+
+  # it "should have major_alert" do
+  #   first_alert.major_alert.should be_true or first_alert.major_alert.should be_false
+  # end  
+
+  it "should have routes affected" do
+    first_alert.routes_affected.first.name.should be_a(String)
+  end  
+
+  it "should have headline" do
+    first_alert.headline.should be_a(String)
+  end  
 end
